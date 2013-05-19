c0e248494de8b56d397d0e7caebdb9c61712c2e5
Anyfi.net <info@anyfi.net>
2013-05-19 19:57:50 +0200

    Configuration interface for Anyfi.net on consumer Wi-Fi routers.
    The recommended integration for consumer Wi-Fi routers is very similar to that
    of a residential gateway, except that we explicitly refer to "Anyfi.net". The
    end-user can change the 'anyfi_disabled' property on both radio frontend and
    tunnel termination backend, with a single checkbox. The 'anyfi_server' property
    is left under the control of the vendor, or more advanced users/administrators
    with shell access. See http://anyfi.net/documentation#i_routeri for more
    information.

--- products/router/uci-customization.sh
+++ products/router/uci-customization.sh
@@ -4,7 +4,9 @@ MAC=$(uci get wireless.radio0.macaddr | tr 'a-f' 'A-F')
 
 # Set up wireless as open network per default
 uci delete wireless.radio0.disabled
+uci set wireless.radio0.anyfi_server=anyfi.net
 uci set wireless.@wifi-iface[0].ssid="carrierwrt-$(echo $MAC | cut -c10-17 | tr -d ':')"
+uci set wireless.@wifi-iface[0].anyfi_server=anyfi.net
 uci commit wireless
 
 # Set bootstrap as theme in luci
