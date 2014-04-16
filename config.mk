d63450fe6cbbdc1d9ff805194e70d9c039e5b28c
Anyfi Networks <eng@anyfinetworks.com>
2014-04-16 22:23:07 +0200

    Updated OpenWrt and LuCI.

--- config.mk
+++ config.mk
@@ -1,11 +1,11 @@
 
 # OpenWrt SVN revision/branch/tag
-CONFIG_OPENWRT_PATH = tags/attitude_adjustment_12.09
-CONFIG_OPENWRT_REV = 36422
+CONFIG_OPENWRT_PATH = branches/attitude_adjustment
+CONFIG_OPENWRT_REV = 40431
 
 # LuCI SVN revision/branch/tag
 CONFIG_LUCI_PATH = branches/luci-0.11
-CONFIG_LUCI_REV = 9672
+CONFIG_LUCI_REV = 9964
 
 # Base configuration
 CONFIG =
