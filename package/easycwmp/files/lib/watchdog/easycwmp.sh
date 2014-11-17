19cd612f95d70e5174d2596161d3ab406f53a7f2
CarrierWrt.org <info@carrierwrt.org>
2014-11-17 19:13:48 +0100

    Watchdog now tries restarting easycwmpd a few times.

--- package/easycwmp/files/lib/watchdog/easycwmp.sh
+++ package/easycwmp/files/lib/watchdog/easycwmp.sh
@@ -1,17 +1,19 @@
 
 append HEALTHCHECKS easycwmp_staying_alive
 
+EASYCWMP_RESTARTS=0
+
 easycwmp_is_running() {
-	ps | grep -v 'grep' | grep -q 'easycwmp'
+	test -n "$(pidof easycwmpd)"
 }
 
 easycwmp_is_configured() {
-	uci get easycwmp.@local[0].interface	2>&1 > /dev/null || return 1
-	uci get easycwmp.@local[0].port		2>&1 > /dev/null || return 1
+	uci get easycwmp.@local[0].interface 2>&1 > /dev/null || return 1
+	uci get easycwmp.@local[0].port      2>&1 > /dev/null || return 1
 	
-	uci get easycwmp.@acs[0].scheme		2>&1 > /dev/null || return 1
-	uci get easycwmp.@acs[0].hostname	2>&1 > /dev/null || return 1
-	uci get easycwmp.@acs[0].port		2>&1 > /dev/null || return 1
+	uci get easycwmp.@acs[0].scheme      2>&1 > /dev/null || return 1
+	uci get easycwmp.@acs[0].hostname    2>&1 > /dev/null || return 1
+	uci get easycwmp.@acs[0].port        2>&1 > /dev/null || return 1
 	
 	return 0
 }
@@ -19,8 +21,15 @@ easycwmp_is_configured() {
 easycwmp_staying_alive() {
 	if ! easycwmp_is_running; then
 		# If it was configured then it must have died:
-		easycwmp_is_configured && return 1
+		if easycwmp_is_configured; then
+			EASYCWMP_RESTARTS=$(($EASYCWMP_RESTARTS + 1))
+			/etc/init.d/easycwmpd start
+		fi
 	fi
 	
-	return 0
+	if [ "$EASYCWMP_RESTARTS" -gt 5 ]; then
+		return 1
+	else
+		return 0
+	fi
 }
